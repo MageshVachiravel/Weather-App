6f9975e6142b0abea082fc39929d338baa7a7296
MageshVachiravel <mageshvachiravel@gmail.com>
2024-02-19 07:15:07 +0530

    docs: completed UI design for weather app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 const App = () =>{
   return (
-    <div>
-      
+    <div className="bg-black p-16">
+      <div className="bg-green-500 p-10 rounded">
+        <h1 className="text-2xl font-semibold">Weather Report</h1>
+        <p>I can give you a weather report about your city !</p>
+        <input type="text" placeholder="Enter your City Name" className="mt-3 px-2 py-1 rounded block outline-none" />
+        <button className="bg-black text-white px-2 py-1 rounded my-3">Get Report</button>
+        <p className="font-bold text-md">Weather : </p>
+        <p className="font-bold text-md">Temperature : </p>
+        <p className="font-bold text-md">Description : </p>
+      </div>
     </div>
   )
 }
