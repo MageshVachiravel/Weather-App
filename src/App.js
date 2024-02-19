31b155deb6d2cc1601bd70765f3003cdc3a33cdb
MageshVachiravel <mageshvachiravel@gmail.com>
2024-02-19 07:04:53 +0530

    docs: added tailwindCSS

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () =>{
+  return (
+    <div>
+      <h1>Hi this is react and tailwindcss app</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import reactDOM from 'react-dom/client'
+import "./app.css"
+import App from './App'
+
+
+const root = reactDOM.createRoot(document.querySelector("#root"))
+
+
+root.render(
+  <App></App>
+)
